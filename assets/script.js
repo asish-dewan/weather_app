d7e2683b8d6fe0444d298164e8ef347d8b8bf67a
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-24 03:09:40 +1000

    Added function to call current weather + html layout

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,42 @@
+// Test API
 
-// Call API and 
-var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=09305fa7af376150d681f93c53d839a7"
-var responseText = $("#msg");
+var requestUrl = "https://bulk.openweathermap.org/snapshot/weather_14.json.gz?appid=09305fa7af376150d681f93c53d839a7"
+var responseText = $(".msg");
 
 function getApi(requestUrl) {
-    fetch(requestUrl).then(function (response) {
-    console.log(response);
-    responseText.textContent = response;
+// Call API
+fetch(requestUrl)
+    .then(function (response) {
+// Convert response to JSON string
+    return response.json();
+    })
+// Convert JSON to readable data
+    .then(function (data) {
+    console.log(data);
     });
 }
 
-getApi(requestUrl);
+getApi (requestUrl);
 
 
-/* let weather = {
-    var apiKey= "09305fa7af376150d681f93c53d839a7";
+var getCurrentWeather = function () {
+    // get Current weather bulk files
+    var apiUrl = "https://bulk.openweathermap.org/snapshot/weather_14.json.gz?appid=09305fa7af376150d681f93c53d839a7"
 
-// Fetch data from API
-    fetchWeather= function (){
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=09305fa7af376150d681f93c53d839a7"
-        ).then((response) => response.json())
-        .then(data) => console.log(data));
-    },
+    fetch(apiUrl)
+    .then (function (response) {
+        if (response.status == 200) {
+            console.log("This is working!" + response);
+            response.json()
+            .then (function (data) {
+                console.log (data);
+                displayWeather(data);
+        });
+        }
+    }
+    )
 }
 
-fetchWeather();
- */
+var displayWeather = function (){
+    
+} 
