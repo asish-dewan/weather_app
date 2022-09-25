41ef832c2faa66155cfc26d0487160195c7e2da4
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-25 19:05:57 +1000

    Add function to display current weather

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,30 @@ fetch(requestUrl)
 
 getApi (requestUrl); */
 
-// Test 2 
+// Define Variables
+
+var searchBarEl = $('#search-bar');
+var cityNameEl = $('#city-name');
+var currentWeatherEl = $('#current-weather');
+var currentCity = $('#current-city');
+var currentTemperature = $('#temperature');
+var currentDescription = $('#description');
+var currentHumidity = $('#humidity');
+var currentWind = $('#wind');
+
+// API Key
+
+var APIkey= "09305fa7af376150d681f93c53d839a7";
+
+function displayWeather(event){
+    event.preventDefault();
+    if (cityNameEl.val().trim()!==""){
+        city=cityNameEl.val().trim();
+        getCurrentWeather(city);
+    }
+
+    currentCity.textContent = val(cityNameEl);
+}
 
     // get Current weather bulk files
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Melbourne&unit=metric&appid=09305fa7af376150d681f93c53d839a7';
@@ -35,54 +58,7 @@ getApi (requestUrl); */
         else {
             alert('Error' + response.statusText);
         }
-    });
-    }
+    })
+}
 
 getCurrentWeather ();
-
-
-/* // Define Variables
-
-var searchBarEl = $('#search-bar');
-var nameInputEl = $('#cityname');
-var searchContainerEl = $('#search-container');
-var searchTerm = $('#search-term');
-
-
-// Search bar event 
-
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-
-    var city = nameInputEl.value.trim();
-
-    if (city) {
-        getCurrentWeather(city);
-
-        searchContainerEl.textContent = '';
-        nameInputEl.value = '';
-    } else {
-        alert('Please enter a City name');
-    }
-    };
-
-var getCurrentWeather = function (city) {
-    // get Current weather bulk files
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=09305fa7af376150d681f93c53d839a7';
-
-    fetch(apiUrl)
-    .then (function (response) {
-        if (response.status == 200) {
-            console.log("This is working!" + response);
-            response.json().then (function (data) {
-                console.log (data);
-                //displayWeather(data, city);
-        });
-        }
-        else {
-            alert('Error' + response.statusText);
-        }
-    })
-};
-
- */
